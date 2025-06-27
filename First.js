d7e055bf0589f881845e9f7a2416af5ff1dedb1a
khushwant18 <desaledevesh42@gmail.com>
2025-06-27 19:24:40 +0530

    just for testing

---
+++ First.js
@@ -0,0 +1,65 @@
+//for-of loop
+//  i = 100;
+// let str = "JavaScript";
+
+// let size = 0;
+// for (let val of str) {
+//     console.log("i=", i);
+//     size++;
+
+// }
+
+// console.log("string size = ", size); 
+// let str = "Khushwant";
+// let size = 0;
+// for (let val of str){
+//     console.log("val=", val);
+//     size++;
+// }
+// console.log("string size =", size);
+//for in loop
+// let student = {
+//     name: "RAHUL KUMAR",
+//     age: 22,
+//     cgpa: 7.5,
+//     isPass: true
+// };
+// for(let key in student){
+//     console.log("key=", key, "value=", student[key]);
+// }
+
+
+                          //Practice Qua
+//Q1// for(let num = 0; num <= 100; num++){
+//     if (num % 2 === 0){
+// console.log("num=", num);
+//     }
+    
+// }
+//Q2 
+// let gameNum = 25;
+// let userNum = prompt("Guess the Game number : ");
+
+// while(userNum != gameNum) { //game 
+//     userNum = prompt("Your entred wrong number, Guess again : ");
+// }
+// console.log("congrats, you entered the right number");
+ 
+
+// string
+// let str = "Khushwant";
+
+//Template Literals
+// let sapcialSring = `This is a tempalate literal`;
+// console.log(sapcialSring);
+// let obj = {
+//     item: "pen",
+//     price: 10,
+// };
+// console.log("the cost of", obj.item, "is", obj.price);
+           //Template literals
+// let sapcialSring = `this is a\ttemplate literal ${1 + 2 + 3}`;
+// console.log(sapcialSring);
+let str = "khuSHwant";
+str.toUpperCase();
+console.log(str.toUpperCase);
